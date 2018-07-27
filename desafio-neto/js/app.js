842a7f21b740d300a7c36d0175272b2e1c39b19e
Arthur Tavares <tavares593@gmail.com>
2018-07-27 11:58:50 -0300

    Added animation to the cover picture

--- desafio-neto/js/app.js
+++ desafio-neto/js/app.js
@@ -1,6 +1,6 @@
 const xhr = new XMLHttpRequest();
-const PRIV_KEY = '22b9ec5fccada12f519cd646152c9fe3fb1414b8';
-const PUBLIC_KEY = '4abbfa04a939ae834189993c17551473';
+const PRIV_KEY = 'xxx';
+const PUBLIC_KEY = 'xxx';
 const ts = new Date().getTime();
 const hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
 const url = 'https://gateway.marvel.com:443/v1/public/characters?name=';
@@ -15,11 +15,13 @@ document.getElementById("btn").onclick = () => {
         console.log(text);
         document.getElementById('txt').value = '';
         document.getElementById('cardDead').style.visibility = 'visible';
+        document.getElementById('cardDead').className = 'cardanimation'
         document.getElementById('coverpic').src = text.data.results[0].thumbnail.path + '/portrait_uncanny.jpg';
         document.getElementById('shower').innerHTML = '';
         document.getElementById('charname').innerHTML = text.data.results[0].name;
     };
     
     xhr.send(null);
+    document.getElementById('cardDead').className = 'card'
 }
 
