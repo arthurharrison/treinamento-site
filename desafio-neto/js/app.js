19f5d2b305695a7d0ea1718e4134fa2a3c3415cc
Arthur Tavares <tavares593@gmail.com>
2018-07-31 01:16:57 -0300

    Added background image and working in the display of past searchs'

--- desafio-neto/js/app.js
+++ desafio-neto/js/app.js
@@ -1,9 +1,16 @@
 const xhr = new XMLHttpRequest();
-const PRIV_KEY = 'xxxx';
-const PUBLIC_KEY = 'xxxx';
+const PRIV_KEY = '22b9ec5fccada12f519cd646152c9fe3fb1414b8';
+const PUBLIC_KEY = '4abbfa04a939ae834189993c17551473';
 const ts = new Date().getTime();
 const hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
 const url = 'https://gateway.marvel.com:443/v1/public/characters?name=';
+
+function addPic(pic,text){
+    document.getElementById('log').innerHTML += "<img src='"+pic+ '/portrait_uncanny.jpg' +"'class='cardanimation'id='coverpic'></img>";
+    document.getElementById('log').innerHTML += "<a id='charname'>"+text+"</a>";
+    
+}
+
 
 document.getElementById("btn").onclick = () => {
     document.getElementById('shower').innerHTML = 'Loading...'
@@ -12,15 +19,16 @@ document.getElementById("btn").onclick = () => {
 
     xhr.onload = function () {
         const text = JSON.parse(xhr.response);
-        console.log(text);
+        document.getElementById('log').style.visibility= 'visible';
         document.getElementById('txt').value = '';
-        document.getElementById('cardDead').style.visibility = 'visible';
-        document.getElementById('cardDead').className = 'cardanimation'
-        document.getElementById('coverpic').src = text.data.results[0].thumbnail.path + '/portrait_uncanny.jpg';
+        /*document.getElementById('cardDead').style.visibility = 'visible';
+        document.getElementById('cardDead').className = 'cardanimation';*/
+        addPic(text.data.results[0].thumbnail.path,text.data.results[0].name);
+        //document.getElementById('coverpic').src = text.data.results[0].thumbnail.path + '/portrait_uncanny.jpg';
         document.getElementById('shower').innerHTML = '';
-        document.getElementById('charname').innerHTML = text.data.results[0].name;
+        //document.getElementById('charname').innerHTML = text.data.results[0].name;
     };
 
     xhr.send(null);
-    document.getElementById('cardDead').className = 'card'
+    //document.getElementById('cardDead').className = 'card'
 }
