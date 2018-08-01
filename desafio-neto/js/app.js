0c2c98fb61ebcd5fa885f67d62a7d4125fac15b3
Arthur Harrison <tavares593@gmail.com>
2018-08-01 17:58:04 -0300

    Worked in the new challenges made by Neto, Added a clickable function in the footer text and refactored some of the js code

--- desafio-neto/js/app.js
+++ desafio-neto/js/app.js
@@ -13,8 +13,24 @@ function addPic(pic,text){
     
 }
 
+function changer(){
+    const logo = document.getElementsByClassName('logo')[0];
+    if(document.getElementsByClassName('bottext')[0].innerHTML=='DC É Superior'){
+        logo.src = 'https://cdn.ome.lt/vl309CswdoRsJnwPxDfe1DaqT2E=/837x0/smart/uploads/conteudo/fotos/dc-logo.jpg';
+        document.getElementsByClassName('bottext')[0].innerHTML = 'Marvel É Superior';
+        document.getElementsByClassName('topnav')[0].style.background = '#003f98';
+        document.getElementsByClassName('botnav')[0].style.background = '#ed1d24';
+    }
+    else{
+        document.getElementsByClassName('logo')[0].src = 'https://logodownload.org/wp-content/uploads/2017/05/marvel-logo.png';
+        document.getElementsByClassName('bottext')[0].innerHTML = 'DC É Superior';
+        document.getElementsByClassName('topnav')[0].style.background = '#ed1d24';
+        document.getElementsByClassName('botnav')[0].style.background = '#003f98';
+    }
+    
+}
 
-document.getElementById("btn").onclick = () => {
+document.getElementById("btn").addEventListener("click", () => {
     document.getElementById('shower').innerHTML = 'Loading...'
     const name = document.getElementById('txt').value;
     xhr.open('GET', url+name+'&ts='+ts+'&apikey='+PUBLIC_KEY+'&hash='+hash, true);
@@ -32,5 +48,32 @@ document.getElementById("btn").onclick = () => {
     };
 
     xhr.send(null);
+    
+    //document.getElementById('cardDead').className = 'card'
+});
+
+Array.from(document.getElementsByClassName('hist')).forEach( e => e.style.backgroundColor = 'white');
+/*
+document.getElementById("btn").onclick = () => {
+    document.getElementById('shower').innerHTML = 'Loading...'
+    const name = document.getElementById('txt').value;
+    xhr.open('GET', url+name+'&ts='+ts+'&apikey='+PUBLIC_KEY+'&hash='+hash, true);
+
+    xhr.onload = function () {
+        const text = JSON.parse(xhr.response);
+        document.getElementById('log').style.visibility= 'visible';
+        document.getElementById('txt').value = '';
+        // document.getElementById('cardDead').style.visibility = 'visible';
+        // document.getElementById('cardDead').className = 'cardanimation';
+        addPic(text.data.results[0].thumbnail.path,text.data.results[0].name);
+        //document.getElementById('coverpic').src = text.data.results[0].thumbnail.path + '/portrait_uncanny.jpg';
+        document.getElementById('shower').innerHTML = '';
+        //document.getElementById('charname').innerHTML = text.data.results[0].name;
+    };
+
+    xhr.send(null);
+    
     //document.getElementById('cardDead').className = 'card'
 }
+*/
+
